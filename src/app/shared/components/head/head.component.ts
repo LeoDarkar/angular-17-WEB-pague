174ec9e2c58063a477c036d7305c42c5e12463b1
LeoDarkar <leyundavid@gmail.com>
2024-11-19 02:33:00 -0500

    cambios en el diseño y agregamos varios componentes compuestos
    se quitaron las rutas ya que se simplifico al agregar otra propiedad del grid en el sections.component.html, también se agregaron diseños para las presentaciones y se agrego formulario y presentación de la empresa

--- src/app/shared/components/head/head.component.ts
+++ src/app/shared/components/head/head.component.ts
@@ -2,12 +2,11 @@ import { Component,Output, EventEmitter } from '@angular/core';
 import Section1Component from '../../../business/sections/section1/section1.component';
 import Section2Component from '../../../business/sections/section2/section2.component';
 import Section3Component from '../../../business/sections/section3/section3.component';
-import {RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-head',
   standalone: true,
-  imports: [Section1Component,Section2Component,Section3Component, RouterLink],
+  imports: [Section1Component,Section2Component,Section3Component],
   templateUrl: './head.component.html',
   styleUrl: './head.component.css'
 })
--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,12 +1,11 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [CommonModule,RouterLink, RouterLinkActive],
+  imports: [CommonModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
